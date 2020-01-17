62779612a2aaa298cf7a60baa4c6d16f51177359
ttetrafon <ttetrafon@yahoo.gr>
2020-01-17 20:45:46 +0000

    (C#) Simple console terrain map

---
+++ (Cs) algorithms & data structures/game_console_map/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace game_console_map {
+	class Program {
+		static void Main( string[] args ) {
+
+			TerrainEnum[,] map = new TerrainEnum[3, 3] {
+				{ TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.WALL },
+				{ TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL },
+				{ TerrainEnum.SAND, TerrainEnum.WATER, TerrainEnum.WALL }
+			};
+
+			Console.OutputEncoding = UTF8Encoding.UTF8;
+
+			for (int i = 0; i < map.GetLength(1); i++ ) {
+				for (int j = 0; j < map.GetLength(0); j++ ) {
+					Console.ForegroundColor = map[i, j].GetColour();
+					Console.Write( map[i, j].GetSymbol() + " " );
+				}
+				Console.WriteLine( "" );
+			}
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.WriteLine( "Press any key to exit..." );
+			Console.ReadKey();
+		}
+	}
+}
---
+++ (Cs) algorithms & data structures/game_console_map/TerrainEnum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_console_map {
+
+	public enum TerrainEnum {
+		GRASS,
+		SAND,
+		WATER,
+		WALL
+	}
+
+	public static class TerrainEnumExtensions {
+
+		public static ConsoleColor GetColour( this TerrainEnum terrain ) {
+			switch ( terrain ) {
+				case TerrainEnum.GRASS:
+					return ConsoleColor.Green;
+				case TerrainEnum.SAND:
+					return ConsoleColor.Yellow;
+				case TerrainEnum.WATER:
+					return ConsoleColor.Blue;
+				default:
+					return ConsoleColor.DarkGray;
+			}
+		}
+
+		public static char GetSymbol( this TerrainEnum terrain ) {
+			switch ( terrain ) {
+				case TerrainEnum.GRASS:
+					return '\u201c';
+				case TerrainEnum.SAND:
+					return '\u25cb';
+				case TerrainEnum.WATER:
+					return '\u2248';
+				default:
+					return '\u25cf';
+			}
+		}
+
+	}
+}
