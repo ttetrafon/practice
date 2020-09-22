ba3689f34cab714a84235f2252d7a3f4f5f16196
tryfon_titonis <tryfon.titonis@polarmoment.co.uk>
2020-09-22 13:05:03 +0100

    (js) time ++

---
+++ (js) time/create data.js
@@ -0,0 +1,5 @@
+var date = "2020-10-19";
+var time = '12:10';
+
+var datetime = new Date(date + ' ' + time);
+console.log(datetime);
---
+++ (js) time/date length.js
@@ -0,0 +1,4 @@
+var start = new Date("2020-05-01 00:00");
+var end = new Date("2020-05-01 23:59");
+
+console.log(end - start);
