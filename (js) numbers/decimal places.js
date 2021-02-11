2f25b1290aaed5b8bf336d0f7ff48d0b3f0bb8f6
tryfon_titonis <ttetrafon@yahoo.gr>
2021-02-11 14:30:37 +0000

    (js) check for decimals

---
+++ (js) numbers/decimal places.js
@@ -0,0 +1,17 @@
+var num = [1, 1.1, 1.11, 1.111, 1.1111, 1.11111, 1.111111, 1.1111111];
+var maxDecimalPlacess = 5;
+
+function NumberHasLessDecimalPlaces(number, maxDecimalPlacess) {
+    var mult = Math.pow(10, maxDecimalPlacess);
+    console.log('mult:', mult);
+    var num = number * mult;
+    console.log('num', num);
+    num = Math.round(num * 10) /10;
+    console.log('num (round 1 decimal)', num);
+    return (num % 1) === 0;
+}
+
+for (var i = 0; i < num.length; i++) {
+    console.log('-------------------------------------------');
+    console.log(NumberHasLessDecimalPlaces(num[i], maxDecimalPlacess));
+}
