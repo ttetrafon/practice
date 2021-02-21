4743f433ec613e98136d133e22c9ca9d8f4dff77
tryfon_titonis <ttetrafon@yahoo.gr>
2021-02-21 19:00:12 +0000

    js strings ++

--- (js) strings/substrings.js
+++ (js) strings/substrings.js
@@ -1,3 +1,11 @@
-var terminal = "Gym 1 (AB123456)";
-var id = terminal.substring(terminal.lastIndexOf('(') + 1, terminal.lastIndexOf(')'));
-console.log(id);
+// var terminal = "Gym 1 (AB123456)";
+// var id = terminal.substring(terminal.lastIndexOf('(') + 1, terminal.lastIndexOf(')'));
+// console.log(id);
+
+var error = "Duplicate entry 'GBP-test' for key 'IX_CURRENCY_CODE_ACCOUNT'";
+var closingQuote = error.lastIndexOf("'");
+var openingQuote = error.lastIndexOf("'", closingQuote - 1);
+var parts = error.substring(openingQuote + 1, closingQuote).split("_");
+console.log(
+    parts[parts.length - 1]
+);
