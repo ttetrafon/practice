2380c01914b68e1888b08c0fbd83f65acd193f94
tryfon_titonis <ttetrafon@yahoo.gr>
2021-03-10 16:44:41 +0000

    (js) types++

--- (js) types/types.js
+++ (js) types/types.js
@@ -1,7 +1,10 @@
 var a = 'this is a string';
 var b = 15;
 var c = 1.3;
+var d = [1, 2, 3];
 
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
+console.log(typeof d);
+console.log(Array.isArray(d));
