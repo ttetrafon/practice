76f0b755cc15d5134f2ecf34290cace250c17ab7
tryfon_titonis <ttetrafon@yahoo.gr>
2021-03-19 15:59:49 +0000

    (js) object map ++

--- (js) objects/map-object-array.js
+++ (js) objects/map-object-array.js
@@ -2,3 +2,12 @@ var offices = [{OfficeName: 'Office 1'}, {OfficeName: 'Office 2'}, {OfficeName: 
 console.log(
     offices.map((elem) => { return elem["OfficeName"] })
 );
+
+var releaseDates = {
+    1037:'2021-04-04',
+    1039:'2021-04-04',
+    1041:'2021-04-04'
+}
+console.log(
+    Object.keys(releaseDates).map((el) => { return "when " + el + " then " + releaseDates[el]; })
+);
