5d5ca32ff0253925fd6b8fb2424484aea899b17e
tryfon_titonis <ttetrafon@yahoo.gr>
2021-04-22 18:34:28 +0100

    (python) trees

---
+++ (python) data structures/tress.py
@@ -0,0 +1,57 @@
+class Expr:
+    pass
+
+class Times(Expr):
+    def __init__(self, left, right) -> None:
+        super().__init__()
+        self.left = left
+        self.right = right
+
+    def __str__(self) -> str:
+        return "(" + str(self.left) + " * " + str(self.right) + ")"
+
+    def eval(self, env):
+        return self.left.eval(env) * self.right.eval(env)
+
+class Plus(Expr):
+    def __init__(self, left, right) -> None:
+        super().__init__()
+        self.left = left
+        self.right = right
+
+    def __str__(self) -> str:
+        return "(" + str(self.left) + " + " + str(self.right) + ")"
+
+    def eval(self, env):
+        return self.left.eval(env) + self.right.eval(env)
+
+class Const(Expr):
+    def __init__(self, val) -> None:
+        super().__init__()
+        self.val = val
+
+    def __str__(self) -> str:
+        return str(self.val)
+
+    def eval(self, evn):
+        return self.val
+
+
+class Var(Expr):
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.name = name
+
+    def __str__(self) -> str:
+        return "\"" + str(self.name) + "\""
+
+    def eval(self, evn):
+        return env[self.name]
+
+
+e1 = Times(Const(3), Plus(Var("y"), Var("x")))
+e2 = Plus(Times(Const(3), Var("y")), Var("x"))
+
+env = { "x": 2, "y": 4 }
+print(e1 + " = ")
+print(e2)
