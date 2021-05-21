f0699438c8778ea1c54cd78bd1349c85ea42bcb0
ttetrafon <ttetrafon@yahoo.gr>
2021-05-21 10:24:44 +0100

    (python) caching

---
+++ python/decorators/caching.py
@@ -0,0 +1,17 @@
+from functools import cache, lru_cache
+
+# @cache
+@lru_cache(maxsize=10)
+def fib(n):
+    if n <=1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+def main():
+    for i in range(400):
+        print(i, fib(i))
+    print('done')
+
+main()
+
+print('over...')
