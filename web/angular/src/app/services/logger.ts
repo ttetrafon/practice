a506d6e67efa91c4ad9e2b79f7e492420aeba519
tryfon_titonis <tryfon.titonis@polarmoment.co.uk>
2022-01-14 13:34:37 +0200

    angular++

--- web/angular/src/app/services/logger.ts
+++ web/angular/src/app/services/logger.ts
@@ -6,7 +6,9 @@ import { Injectable } from "@angular/core";
 export class Logger {
     count = 0;
 
-    log(msg: any)   { this.count++; console.log("(" + this.count + ")" + msg); }
-    error(msg: any) { this.count++; console.error("(" + this.count + ")" + msg); }
-    warn(msg: any)  { this.count++; console.warn("(" + this.count + ")" + msg); }
+    constructor() {}
+
+    log(msg: any)   { this.count++; console.log("(" + this.count + ") " + msg); }
+    error(msg: any) { this.count++; console.error("(" + this.count + ") " + msg); }
+    warn(msg: any)  { this.count++; console.warn("(" + this.count + ") " + msg); }
 }
