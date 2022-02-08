a8f3d7b228b379c4d11ae677163d95f05a2f97f7
Tryfon Titonis <tryfon.titonis@polarmoment.co.uk>
2022-02-08 16:07:47 +0000

    (C#) web api with controller

---
+++ Cs/TodoApi/TodoApi/Program.cs
@@ -0,0 +1,32 @@
+// Requires: dotnet tool install -g Microsoft.dotnet-httprepl (run in cmd)
+
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
