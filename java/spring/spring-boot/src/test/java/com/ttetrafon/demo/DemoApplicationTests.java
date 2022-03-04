3268c4c6f40f9d5320c88a4d5a1739375d8d7f61
Tryfon Titonis <tryfon.titonis@polarmoment.co.uk>
2022-03-04 12:40:08 +0000

    (java) spring boot ++

--- java/spring/spring-boot/src/test/java/com/ttetrafon/demo/DemoApplicationTests.java
+++ java/spring/spring-boot/src/test/java/com/ttetrafon/demo/DemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.ttetrafon.demo;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() {}
+
+	@Test
+	public void getInit() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World!")));
 	}
 
 }
