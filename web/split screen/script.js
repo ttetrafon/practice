25d3ab81cabb6eceb7d20ed0691c5c6ebd115b57
ttetrafon <ttetrafon@yahoo.gr>
2022-03-24 23:45:54 +0000

    css++

---
+++ web/split screen/script.js
@@ -0,0 +1,19 @@
+console.log("main script started...");
+document.addEventListener('DOMContentLoaded', function() {
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+
+  if(wrapper.className.indexOf('skewed') != -1){
+      skew = 1000;
+  }
+
+  wrapper.addEventListener('mousemove', function(e){
+    // console.log(e.clientX, e.clientY); // gets the mouse position
+    delta = (e.clientX - window.innerWidth / 2) * 0.5;
+    handle.style.left = e.clientX + delta + 'px';
+    topLayer.style.width= e.clientX + skew + delta + 'px';
+  });
+});
