5f924af146fe760ceaa41a24fcbb44dd87f9232a
ttetrafon <ttetrafon@yahoo.gr>
2022-04-23 13:29:39 +0100

    (js) arbitrary property update

--- js/objects/arbitrary property selection.js
+++ js/objects/arbitrary property selection.js
@@ -9,12 +9,21 @@ let user = {
   }
 }
 
-let prop = "contact.email.work".split('.');
-console.log(prop);
+let prop1 = "contact.email.work".split('.');
+let prop2 = "contact.email.work".split('.');
 
 function get(obj, prop) {
   if (prop.length == 0) return obj;
   else return get(obj[prop.shift()], prop);
 }
-let res = get(user, prop);
+let res = get(user, prop1);
 console.log("... got: ", res);
+
+function update(obj, prop, value) {
+  if (prop.length == 1) {
+    obj[prop] = value;
+  }
+  else return update(obj[prop.shift()], prop, value);
+}
+update(user, prop2, 'new-email@mail.com');
+console.log(user);
