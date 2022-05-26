98bbf1e03269a72ffd63a207f2fed731cfccf0b7
ttetrafon <ttetrafon@yahoo.gr>
2022-05-26 18:37:55 +0100

    python++

--- python/virtual environments/virtual env.py
+++ python/virtual environments/virtual env.py
@@ -1,4 +1,11 @@
-# Create: python -m venv environment_name
-# Activate: environment_name/Scripts/Activate.ps1
-# ... work
-# Deactivate: deactivate
+# (1) Create: python -m venv env
+#     !`env` is just a name, it can be anything.
+# (2) Activate: env/Scripts/Activate.ps1
+# (3) ... work
+# (4) Deactivate: deactivate
+
+# For git:
+# (1) Add the environment folder in .gitignore
+# (2) Deactivate the environment (if active)
+# (3) Run `pit freeze > requirements.txt` to store any installed packages.
+# (4) Commit this file with the repository.
