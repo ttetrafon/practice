7157ecf80a419965adf2c475d253ab5ca94e6ebb
ttetrafon <ttetrafon@yahoo.gr>
2022-05-26 13:19:10 +0100

    (js) string replace ++

--- js/strings/replace.js
+++ js/strings/replace.js
@@ -1,3 +1,9 @@
-str = 'The quick brown fox. Is brown even a colour?';
-
+let str = 'The quick brown fox. Is brown even a colour?';
 console.log(str.replace(/brown/g, 'yellow'));
+
+let numbers = 'If I had 1 basket with 8 eggs and a fox stole 5 of them, do I still have 1 basket of eggs?';
+console.log(
+  numbers.replace(/\d/g, number => {
+    return (parseInt(number, 10) % 2 == 1 ? '@' : number);
+  })
+);
