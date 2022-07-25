6feb876a65d54dab04fca94dd8907bfc136bab08
ttetrafon <ttetrafon@yahoo.gr>
2022-07-25 16:16:16 +0300

    (java)++

--- java/spring/spring-boot/src/main/java/com/ttetrafon/demo/configuration/ObjectMapperConfiguration.java
+++ java/spring/spring-boot/src/main/java/com/ttetrafon/demo/configuration/ObjectMapperConfiguration.java
@@ -2,7 +2,6 @@ package com.ttetrafon.demo.configuration;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
