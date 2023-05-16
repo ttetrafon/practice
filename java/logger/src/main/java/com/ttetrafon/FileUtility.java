4c4610480f3264a263320a53efbec8b1ac08611e
ttetrafon <ttetrafon@yahoo.gr>
2023-05-16 10:37:49 +0300

    (java) logger++

--- java/logger/src/main/java/com/ttetrafon/FileUtility.java
+++ java/logger/src/main/java/com/ttetrafon/FileUtility.java
@@ -2,16 +2,21 @@ package com.ttetrafon;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 
 public class FileUtility {
   public static BufferedWriter openOutputStream(String filename) {
     BufferedWriter bw = null;
     try {
-      File file = new File("D:\\OneDrive\\Documents\\self-practice\\java\\logger\\" + filename);
+      String path = "D:\\OneDrive\\Documents\\self-practice\\java\\logger\\output\\";
+      File dir = new File(path);
+      boolean dirExists = dir.exists();
+      if (!dirExists) {
+        dirExists = dir.mkdir();
+      }
+      File file = new File(path + filename);
       boolean fileExists = file.exists();
-      if (!fileExists) {
+      if (dirExists && !fileExists) {
         fileExists = file.createNewFile();
       }
       if (fileExists) {
