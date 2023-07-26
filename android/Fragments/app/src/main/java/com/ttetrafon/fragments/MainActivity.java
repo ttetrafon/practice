b91ad66718c026fcaa424fbfdbcd695ea75be052
ttetrafon <ttetrafon@yahoo.gr>
2023-07-26 14:56:36 +0100

    (android) fragments

--- android/Fragments/app/src/main/java/com/ttetrafon/fragments/MainActivity.java
+++ android/Fragments/app/src/main/java/com/ttetrafon/fragments/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ttetrafon.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * https://developer.android.com/guide/fragments
@@ -17,6 +21,8 @@ import android.os.Bundle;
  * - can implement behaviours without UI
  * - (minimum API 30)
  *
+ * Fragments are added within a 'fragment_view' in the layout.
+ *
  * Types of Fragments:
  * - single frame: only one fragment shown as a view
  * - list: have a list view (like a menu)
@@ -28,5 +34,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("...", "---> MainActivity.onCreate()");
+
+        Button buttonNews = (Button) findViewById(R.id.buttonNews);
+        Button buttonMap = (Button) findViewById(R.id.buttonMap);
+        Button buttonSettings = (Button) findViewById(R.id.buttonSettings);
+
+        // Fragments are switched with the help of 'Fragment Manager'.
+        // https://developer.android.com/guide/fragments/fragmentmanager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        buttonNews.setOnClickListener(view -> replaceFragment(fragmentManager, FragmentNews.class));
+        buttonMap.setOnClickListener(view -> replaceFragment(fragmentManager, FragmentMap.class));
+        buttonSettings.setOnClickListener(view -> replaceFragment(fragmentManager, FragmentSettings.class));
+    }
+
+    public boolean replaceFragment(FragmentManager fragmentManager, Class<? extends androidx.fragment.app.Fragment> fragmentClass) {
+        Log.d("...", "---> replaceFragment()");
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragmentClass, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("settings")
+                .commit();
+        return true;
     }
 }
