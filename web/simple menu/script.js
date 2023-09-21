0b536736ac26280f4c3556d52d8af8c95a15b0a3
ttetrafon <ttetrafon@yahoo.gr>
2023-09-21 20:41:31 +0300

    (web) simple menu

---
+++ web/simple menu/script.js
@@ -0,0 +1,11 @@
+const title = document.querySelector("title");
+console.log(title.text + " script started...");
+
+const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
