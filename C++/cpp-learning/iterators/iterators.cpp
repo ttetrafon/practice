d85a3a4ca7d27c6db693c2c86bd58e17819b94f9
ttetrafon <ttetrafon@yahoo.gr>
2023-11-13 17:57:29 +0200

    (c++)++

--- C++/cpp-learning/iterators/iterators.cpp
+++ C++/cpp-learning/iterators/iterators.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 using namespace std;
 
 // Iterators are positions, and they are used to operate on sequences of any type.
+// Types of operators:
+// - input: can be dereferenced only once per position; can only advance; advances a single position at a time
+// - forward: as input, but can be dereferenced multiple times per position
+// - bidirectional: like forwards, but can backtrack one position at a time
+// - random access: like bidirectional, but can move forwards and backwards any number of positions at a time
+// - contiguous (C++ 17): random access, with that guarantees underlying data are contiguous in memory
+
+// Insert operations do that in the position of the iterator.
+// Erase operations will return an iterator corresponding to the same position as the one passed in.
+// An iterator and its corresponding reverse iterator are located in the same position between elements.
+// An iterator is invalidated if its position is no longer a part of a sequence.
 
 int main()
 {
@@ -29,8 +41,35 @@ int main()
   // Distance between operators on the same sequence
   cout << "(my_vector.begin(), my_iterator) = " << distance(my_vector.begin(), my_iterator) << endl;
 
-  // Insert operations do that in the position of the iterator.
-  // Erase operations will return an iterator corresponding to the same position as the one passed in.
-  // An iterator and its corresponding reverse iterator are located in the same position between elements.
-  // An iterator is invalidated if its position is no longer a part of a sequence.
+  // vector operator
+  // - begin() returns an iterator to the first element in sequence container
+  // - end() returns an iterator to the first element past the end
+  vector<int> v = { 11, 22, 33, 44, 55 };
+  for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+
+  // map iterator
+  // - if a map object is const qualified, the function returns a `const_iterator`, otherwise a generic `iterator`
+  map<char, int> my_map;
+  my_map['a'] = 100;
+  my_map['b'] = 300;
+  my_map['c'] = 200;
+  for (map<char, int>::iterator it = my_map.begin(); it != my_map.end(); ++it) {
+    cout << it->first << " -> " << it->second << endl;
+  }
+
+  // reverse iterator
+  // - a bidirectional/random access iterator
+  // - use `rbegin()` and `rend()` to get start and finish positions for reverse operations
+  for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  // - can be chaned to a forward iterator via `.base()`
+  vector<int>::reverse_iterator r_v_it = v.rbegin();
+  vector<int>::iterator f_v_it = r_v_it.base();
+
+
 }
