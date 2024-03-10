af25470fc6fb62bd58f0acca3b90ce75fe39364b
ttetrafon <ttetrafon@yahoo.gr>
2024-03-10 16:44:55 +0200

    (web) fetch json resources

--- web/fetch json/script.js
+++ web/fetch json/script.js
@@ -1,2 +1,9 @@
 const title = document.querySelector("title");
 console.log(title.text + " script started...");
+
+let p = document.querySelector("p");
+fetch("./data.json")
+  .then(res => res.json())
+  .then((json) => {
+    p.innerText = json.text;
+  });
