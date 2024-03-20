1b8e489eca6239c4cfe732d501e0df1955385215
ttetrafon <ttetrafon@yahoo.gr>
2024-03-20 23:15:21 +0200

    (js)++

---
+++ js/lists/combinations.js
@@ -0,0 +1,24 @@
+arrays = [
+  ['a', 'b', 'c'],
+  [1, 2, 3, 4],
+  ['α', 'β'],
+  ['000', '555', '888']
+];
+
+function allPossibleCases(arr) {
+  if (arr.length == 1) {
+    return arr[0];
+  }
+  else {
+    var result = [];
+    var allCasesOfRest = allPossibleCases(arr.slice(1)); // recur with the rest of array
+    for (var i = 0; i < allCasesOfRest.length; i++) {
+      for (var j = 0; j < arr[0].length; j++) {
+        result.push(arr[0][j] + "," + allCasesOfRest[i]);
+      }
+    }
+    return result;
+  }
+}
+
+console.log(allPossibleCases(arrays));
