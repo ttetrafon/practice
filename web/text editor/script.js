6a879efb7d36dd955ef2ea09c4c3a5f50cb88c66
ttetrafon <ttetrafon@yahoo.gr>
2024-05-16 23:09:14 +0300

    [web] text editor (part I)

--- web/text editor/script.js
+++ web/text editor/script.js
@@ -1,21 +1,2 @@
-// console.log("main script started...");
-
-const elements = document.querySelectorAll('.btn');
-// console.log("elements:", elements);
-
-elements.forEach(element => {
-  element.addEventListener("click", _ => {
-    // console.log("clicked:", _.target);
-    let command = element.dataset['element'];
-    console.log("... command:", command);
-
-    if (command === 'createLink' || command === 'insertImage') {
-      let url = prompt("Enter URL:", "https://");
-      document.execCommand(command, false, url);
-    }
-    else {
-      document.execCommand(command, false, null);
-    }
-  });
-
-});
+const title = document.querySelector("title");
+console.log(title.text + " script started...");
