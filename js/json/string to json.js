79245d4814816b0ea5ca365a87a819e22ff806c9
ttetrafon <ttetrafon@yahoo.gr>
2024-07-17 23:41:39 +0300

    (gc) firebase project initialisation

---
+++ js/json/string to json.js
@@ -0,0 +1,5 @@
+string = "[{\"ps-event\":\"Test Team 1 v Test Team 2\",\"ps-event-id\":\"90000001\",\"ps-competition\":\"Test League\",\"ps-category\":\"Test Sport\",\"ps-market\":\"Both Teams to Score\",\"ps-handicap\":null,\"ps-runner\":\"Yes\",\"ps-price\":3.84,\"bf-event\":\"Test Team 1 v Test Team 2\",\"bf-market\":\"Both teams to Score?\",\"bf-market-id\":\"1.568453643\",\"bf-runner\":\"No\",\"bf-handicap\":0.0,\"bf-price\":2.01,\"sure-bet-check\":0.7579291045}]";
+
+console.log(
+  JSON.parse(string)
+);
