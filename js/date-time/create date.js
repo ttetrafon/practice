78fad3475bb620a1ef4a0db9bc62ebd4fe8954b9
ttetrafon <ttetrafon@yahoo.gr>
2024-09-22 16:48:42 +0300

    [js] date-times++

--- js/date-time/create date.js
+++ js/date-time/create date.js
@@ -1,11 +1,18 @@
-var date = "2020-10-19";
-var time = '12:10';
+let date = "2020-10-19";
+let time = '12:10';
 
-var datetime = new Date(date + ' ' + time);
+let datetime = new Date(date + ' ' + time);
 console.log(datetime);
 
 console.log("---------------------------------------------------------------------------------------");
 
-var now = new Date();
+let now = new Date();
 
 console.log(now.toISOString());
+
+console.log("---------------------------------------------------------------------------------------");
+
+let then = new Date(1727008096 * 1000);
+console.log(then.toISOString());
+console.log(then.toISOString().substring(11, 19));
+console.log(`${(then.getHours() < 10 ? "0" + then.getHours() : then.getHours())}:${(then.getMinutes() < 10 ? "0" + then.getMinutes() : then.getMinutes())}:${then.getSeconds() < 10 ? "0" + then.getSeconds() : then.getSeconds()}`);
