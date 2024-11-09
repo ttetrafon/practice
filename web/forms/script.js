c6599adbfcdd18fc6ac1ec3ae323a231e5b1800f
ttetrafon <ttetrafon@yahoo.gr>
2024-11-09 21:29:55 +0200

    [web]++

--- web/forms/script.js
+++ web/forms/script.js
@@ -1,2 +1,6 @@
 const title = document.querySelector("title");
 console.log(title.text + " script started...");
+
+// Can get number fields as numbers directly
+const ageField = document.getElementById("age");
+let age = ageField.valueAsNumber;
