19644441a40e9077fc658d10aa7c4ce66859db1e
titonis <titonis@itsaur.com>
2024-11-19 12:44:58 +0200

    [java]++

---
+++ java/playground/src/main/java/com/ttetrafon/lists/Cover.java
@@ -0,0 +1,11 @@
+package com.ttetrafon.lists;
+
+import java.util.List;
+
+public class Cover {
+    List<Integer> list;
+
+    public Cover(List<Integer> list) {
+        this.list = list;
+    }
+}
---
+++ java/playground/src/main/java/com/ttetrafon/lists/Main.java
@@ -0,0 +1,18 @@
+package com.ttetrafon.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Cover> outer = List.of(
+                new Cover(List.of(1, 2)),
+                new Cover(List.of(3, 4, 5))
+        );
+        List<Integer> res = outer.stream()
+                .flatMap(c -> c.list.stream())
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
+}
