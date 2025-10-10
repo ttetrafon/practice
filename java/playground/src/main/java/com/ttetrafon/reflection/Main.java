8a33389ef00d4615db99d01e19c2b4b125338fa6
titonis <titonis@itsaur.com>
2025-10-10 11:32:44 +0300

    [java]++

--- java/playground/src/main/java/com/ttetrafon/reflection/Main.java
+++ java/playground/src/main/java/com/ttetrafon/reflection/Main.java
@@ -3,9 +3,15 @@ package com.ttetrafon.reflection;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Outer.class);
-        System.out.println(Outer.class);
+        System.out.println(Outer.Inner.class);
 
-        System.out.println(Outer.Inner.class);
-        System.out.println(Outer.Inner.class);
+        Type type = null;
+        if (type instanceof Type.TypeAlpha) {
+            System.out.println("TypeAlpha");
+        } else if (type instanceof Type.TypeBeta) {
+            System.out.println("TypeBeta");
+        } else {
+            System.out.println("... unknown type");
+        }
     }
 }
---
+++ java/playground/src/main/java/com/ttetrafon/reflection/Type.java
@@ -0,0 +1,12 @@
+package com.ttetrafon.reflection;
+
+public interface Type {
+
+    public class TypeAlpha implements Type {
+
+    }
+
+    public class TypeBeta implements Type {
+
+    }
+}
