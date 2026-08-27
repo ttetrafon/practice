189483f0d8f19be066f615f01c4b32428a30bdb9
ttetrafon <ttetrafon@yahoo.gr>
2026-08-27 13:01:43 +0300

    [web] web-components++

--- coding/web/web components/---.js
+++ coding/web/web components/---.js
@@ -5,7 +5,7 @@ template.innerHTML = /*html*/`
   @import './styles.css';
 </style>
 
-<div>...</div>
+<div id="an-id" title="">...</div>
 `;
 
 class Component extends HTMLElement {
@@ -14,8 +14,14 @@ class Component extends HTMLElement {
     this._shadow = this.attachShadow({ mode: 'closed' });
     // The mode can be set to 'open' if we need the document to be able to access the shadow-dom internals.
     // Access happens through ths `shadowroot` property in the host.
+
     this._shadow.appendChild(template.content.cloneNode(true));
+
+    this.$div = this._shadow.getElementById("an-id");
   }
+
+  // We create and use custom attributes with `.hasAttribute("attr")`, `.getAttribute("attr")`, `.setAttribute("attr")`, and `.removeAttribute("attr")`.
+  // Note that custom attributes, like normal ones, have case-insensitive names and their values are always strings.
 
   // Attributes need to be observed to be tied to the lifecycle change callback.
   static get observedAttributes() { return ['label', 'data']; }
@@ -31,7 +37,10 @@ class Component extends HTMLElement {
   attributeChangedCallback(name, oldVal, newVal) {
     // Attribute value changes can be tied to any type of functionality through the lifecycle methods.
     if (oldVal == newVal) return;
-    switch(name) {
+    switch (name) {
+      case 'label':
+        this.$div.title = label();
+        break;
       default:
         break;
     }
@@ -43,7 +52,7 @@ class Component extends HTMLElement {
     // Triggered when the component is removed from the DOM.
     // Ideal place for cleanup code.
     // Note that when destroying a component, it is good to also release any listeners.
-}
+  }
   adoptedCallback() {
     // Triggered when the element is adopted through `document.adoptElement()` (like when using an <iframe/>).
     // Note that adoption does not trigger the constructor again.
